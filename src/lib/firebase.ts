746942e24f3bddfb3c8c88e3ed1d9cadbb9660f2
Ramesh Singh <rs5149404@gmail.com>
2025-06-27 09:04:20 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 // IMPORTANT: Replace with your Firebase project's configuration.
 // You can find this in your Firebase project settings under "Web apps".
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBNLqTkyR3tNKMHhs6G0P_giORHQpUA9WI",
+  authDomain: "echosphere-9mbl1.firebaseapp.com",
+  projectId: "echosphere-9mbl1",
+  storageBucket: "echosphere-9mbl1.appspot.com",
+  messagingSenderId: "631363835926",
+  appId: "1:631363835926:web:71e0e7a16e6ab4a69ebd37",
 };
 
 // Initialize Firebase
